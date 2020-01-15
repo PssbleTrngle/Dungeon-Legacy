eb789c0674065699884dd84802631d6636242f92
PssbleTrngle <nik@somethingcatchy.net>
2020-01-15 11:28:25 +0100

    Fallback initializable with Suppliers

--- src/main/java/possibletriangle/dungeon/helper/Fallback.java
+++ src/main/java/possibletriangle/dungeon/helper/Fallback.java
@@ -1,19 +1,24 @@
 package possibletriangle.dungeon.helper;
 
 import java.util.function.Function;
+import java.util.function.Supplier;
 
-public class Fallback<T> implements Function<Integer,T> {
+public class Fallback implements StateProvider {
 
-    private final Function<Integer,T>[] suppliers;
+    private final StateProvider[] providers;
 
-    public Fallback(Function<Integer,T>... suppliers) {
-        this.suppliers = suppliers;
+    public Fallback(Supplier<BlockState>... suppliers) {
+        this.providers = Arrays.steam(suppliers).map(s -> (StateProvider) i -> s);
+    }
+
+    public Fallback(StateProvider... providers) {
+        this.providers = providers;
     }
 
     @Override
     public T apply(Integer variant) {
-        for(Function<Integer,T> supplier : suppliers) {
-            T t = supplier.apply(variant);
+        for(Function<Integer,T> provider : providers) {
+            T t = provider.apply(variant);
             if(t != null) return t;
         }
 
