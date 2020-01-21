48a5dd727e918ff8ed4f0661ede3cb8b107161f4
PssbleTrngle <nik@somethingcatchy.net>
2020-01-21 10:40:25 +0100

    How did I not see this

--- src/main/java/possibletriangle/dungeon/common/block/BreakableBlock.java
+++ src/main/java/possibletriangle/dungeon/common/block/BreakableBlock.java
@@ -4,13 +4,6 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
-import net.minecraft.client.audio.Sound;
-import net.minecraft.util.BlockRenderLayer;
-import net.minecraft.util.Direction;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IBlockReader;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.ToolType;
 import net.minecraftforge.registries.ObjectHolder;
 import possibletriangle.dungeon.DungeonMod;
@@ -29,21 +22,9 @@ public class BreakableBlock extends Block {
 
     public final ToolType tool;
 
-    public BreakableBlock(Material material, SoundType sound, float hardness, ToolType tool) {
-        super(Properties.create(material)
-                .sound(sound)
-                .harvestTool(tool)
-                .hardnessAndResistance(hardness).noDrops()
-        );
-        this.tool = tool;
-    }
-
     public BreakableBlock(Block parent) {
-        this(parent.getDefaultState());
-    }
-
-    private BreakableBlock(BlockState parent) {
-        this(parent.getMaterial(), parent.getSoundType(), parent.getBlockHardness(null, null), parent.getHarvestTool());
+        super(Block.Properties.from(parent).noDrops());
+        this.tool = tool;
     }
 
 }
