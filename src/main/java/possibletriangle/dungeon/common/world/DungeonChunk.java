c8b87bee77ba282315c3469de75c7e037c287680
PssbleTrngle <nik@somethingcatchy.net>
2020-02-04 12:13:21 +0100

    That was kinda dumb

--- src/main/java/possibletriangle/dungeon/common/world/DungeonChunk.java
+++ src/main/java/possibletriangle/dungeon/common/world/DungeonChunk.java
@@ -99,7 +99,7 @@ public class DungeonChunk {
         String type = nbt.getString("id");
 
         switch (type) {
-            case "minecraft:spawner":
+            case "minecraft:mob_spawner":
                 nbt.putInt("MaxNearbyEntities", 6);
                 nbt.putInt("RequiredPlayerRange", 10);
                 nbt.putInt("SpawnCount", 4);
@@ -124,9 +124,7 @@ public class DungeonChunk {
                 nbt.putString("LootTable", DungeonLoot.Rarity.COMMON.path().toString());
                 nbt.putLong("LootTableSeed", random.nextLong());
                 break;
-
-            default:
-                DungeonMod.LOGGER.info("Unsupported TileEntity: {}", type);
+                
         }
 >>>>>>> Spawn potentials in palettes
 
