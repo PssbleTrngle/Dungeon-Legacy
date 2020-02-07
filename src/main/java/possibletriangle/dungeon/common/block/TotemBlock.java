f2fed7fc72a085fae9a3313254cb1847e4267e2a
PssbleTrngle <nik@somethingcatchy.net>
2020-02-07 09:54:41 +0100

    Save totem state in blockstate & block colors

--- src/main/java/possibletriangle/dungeon/common/block/TotemBlock.java
+++ src/main/java/possibletriangle/dungeon/common/block/TotemBlock.java
@@ -20,8 +20,13 @@ import possibletriangle.dungeon.common.block.tile.TotemTile;
 import javax.annotation.Nullable;
 import java.util.Optional;
 
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 @ObjectHolder("dungeon")
 public class TotemBlock extends ContainerBlock {
+
+    public static final IProperty<State> STATE = PropertyEnum.create(State.class);
+
+    public static final Color INVALID = new Color(175, 17, 17);
 
     @ObjectHolder("totem")
     public static final Block TOTEM = null;
@@ -31,7 +36,31 @@ public class TotemBlock extends ContainerBlock {
                 .hardnessAndResistance(-1.0F, 3600000.0F)
                 .noDrops()
         );
+        this.setDefaultState(this.stateContainer.getBaseState().with(STATE, State.UNCLAIMED));
     }
+    
+
+    @SubscribeEvent
+    public void blockColors(ColorHandlerEvent.Block event) {
+
+        event.getBlockColors().register((s,w,p,i) -> {
+            if(!(s.getBlock() instanceof TotemBlock) || i == 0) return -1;
+            State state = s.get(STATE);
+
+            switch(state) {
+                case UNCLAIMED: return new Color(42,42,42);
+                case INVALID: return INVALID;
+                case CLAIMED: return getTE(w, p).map(TotemTile::getColor).orElse(INVALID);
+            }
+
+        }, TOTEM);
+
+    }
+    
+	@Override
+	protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+		builder.add(STATE);
+	}
 
     @Nullable
     @Override
@@ -52,6 +81,10 @@ public class TotemBlock extends ContainerBlock {
 
     public BlockRenderType getRenderType(BlockState state) {
         return BlockRenderType.MODEL;
+    }
+
+    public enum State {
+        UNCLAIMED, CLAIMED, INVALID;
     }
 
 }
